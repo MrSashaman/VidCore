68b90693ecf69e1c2febe969767e815e48f2f7d7
mrSashaman_ <psasha2811@gmail.com>
2026-06-22 13:54:55 +1200

    new file:   VidCore/Pages/ProfileView.cshtml 	new file:   VidCore/Pages/ProfileView.cshtml.cs 	modified:   VidCore/Pages/VideoView.cshtml 	modified:   VidCore/VidCoreTodo.md 	modified:   VidCore/bin/Debug/net10.0/VidCore.dll 	modified:   VidCore/vidcore.db 	modified:   VidCore/wwwroot/css/site.css

---
+++ VidCore/Pages/ProfileView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VidCore;
+
+namespace VidCore.Pages;
+
+public class ProfileViewModel : PageModel
+{
+
+    
+}
